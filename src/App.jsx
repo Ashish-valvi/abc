e0c8d6acea8f08a5a26169f65c335cb3840264be
Ashish-valvi <ashishvalvi.nvp@gmail.com>
2024-06-02 10:36:21 +0530

    abc

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import BodyBox from "./components/Bodybox";
+import {createBrowserRouter,RouterProvider , Outlet} from "react-router-dom"
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import DetailedCart from "./components/DetailedCart";
+
+
+const App = ()=>(
+  <div id="main-container">
+    <Header/>
+    <Outlet/>
+  </div>
+)
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[ {
+        path:"/",
+        element:<BodyBox/>
+      },
+      {
+        path:"/about",
+        element:<AboutUs/>
+      },
+      {
+        path:"/contact",
+        element:<ContactUs/>
+      },
+      {
+        path:"/detailedcart/:cartid", // here we are sending id to DetailedCart component 
+        element:<DetailedCart/>
+      }
+    ]
+  }
+  
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={router}/>)
+
+export default App;
